6344cb9747cef298cb5a42d465ec410e43d3d290
Rune Madsen <rune@runemadsen.com>
2012-04-24 17:50:40 -0400

    back

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   
   def index
-    @guides = Guide.all(:limit => 20, :order => "created_at DESC", :conditions => 'title != ""')
+    @guides = Guide.all(:limit => 20, :order => "created_at DESC")
   end
   
   def about
