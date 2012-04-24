f47980f7720ac7c83ba71ddca05e91ee57e63c59
Rune Madsen <rune@runemadsen.com>
2012-04-24 13:15:59 -0400

    added order to guide model items

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -1,6 +1,6 @@
 class Guide < ActiveRecord::Base
   belongs_to :user
-  has_many :items
+  has_many :items, :order => 'id DESC'
   accepts_nested_attributes_for :items
   
   def belongs_to?(logged_in_user)
